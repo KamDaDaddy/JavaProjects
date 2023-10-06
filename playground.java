a96ca0f328d4765ed437070426a3bcd751ef9287
KamDaDaddy <97680346+KamDaDaddy@users.noreply.github.com>
2023-10-06 13:21:34 -0400

    lol

---
+++ playground.java
@@ -0,0 +1,12 @@
+public class playground {
+    public static void main(String[] args) {
+        System.out.println("W");
+        System.out.println("X");
+        System.out.print("Y");
+        System.out.print("Z");
+    
+        System.out.println(hello);  // Line 1
+        System.out.print(world);    // Line 2
+    
+    }
+}
