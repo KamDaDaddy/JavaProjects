41981da7169e7541da8117129538b659378141dd
KamDaDaddy <97680346+KamDaDaddy@users.noreply.github.com>
2023-12-04 10:14:15 -0500

    void

---
+++ Unit One/voidMethods/Student.java
@@ -0,0 +1,29 @@
+package voidMethods;
+
+public class Student {
+    int studentID;
+    String first;
+    String middle;
+    String last;
+    int gradeLevel;
+    
+    public Student(int sID, String fN, String lN)
+    {
+        studentID = sID;
+        first = fN;
+        last = lN;
+
+        //Constructor done >:)
+    }
+
+    public Student(int sDI, String fN, String mN, String lN)
+    {
+        studentID = sDI;
+        first = fN;
+        middle = mN;
+        last = lN;
+
+        //Constructor 2 done >:3
+    }
+
+}
---
+++ Unit One/voidMethods/Students.java
@@ -0,0 +1,11 @@
+package voidMethods;
+
+public class Students {
+    
+    public static void main(String[] args) {
+        
+        Student student1 = new Student(35487, "Kamil", "Reyes");
+        System.out.println("Student 1: " + student1);
+    }
+
+}
