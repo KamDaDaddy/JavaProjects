e037f9fa6dc2c2a545326518010eaedfdb41dfb1
KamDaDaddy <kreyes24@aoiths.org>
2024-02-01 20:01:11 -0500

    I did problems from 1 to 4 before struggling :(

---
+++ Conditionals.java
@@ -0,0 +1,116 @@
+import java.io.Console;
+import java.util.Scanner;
+
+public class Conditionals {
+    //Use scanner for numbers, console for string
+        static int num1;
+        static int correctNumber = 25;
+        static int negatedNum;
+        static String weekDay;
+        
+        
+        //Problem 1
+        public static void MathSigns(){
+            if(num1 > 0){
+                System.out.println("The number is positive.");
+            }
+            else if (num1 < 0) {
+                System.out.println("The number is negative.");
+            }
+        
+            else if (num1 == 0)
+            {
+                System.out.println("0 is neither a positive or negative!");
+            }
+        }
+    
+        //Problem 2
+        public static void NumberHunt(){
+            if(correctNumber > 25){
+                System.out.println("More than Enough! Little less this time!");
+            }
+            else if(correctNumber < 25){
+                System.out.println("You'll need a bit more...");
+            }
+            else if (correctNumber == 25){
+                System.out.println("Exactly right!");
+            }
+        }
+
+        //Problem 3
+        public static void CursedTechniqueNumberNegation(){
+            if(negatedNum > 0){
+                System.out.println("Reverse technique: Number negation " + (-1) * negatedNum);
+            }
+            else if(negatedNum < 0){
+                System.out.println("Reverse technique: Number Negation " + (-1) * negatedNum);
+            }
+            else if (negatedNum == 0){
+                System.out.println("The sign of zero isn't affected by the cursed technique: Number Negation.");
+            }
+        }
+
+        //Problem 4
+        public static void WeekDays(){
+            if(weekDay == "Monday"){
+                System.out.println("This is the 1st day of the week.");
+            }
+            else if(weekDay == "Tuesday"){
+                System.out.println("This is the 2nd day of the week.");
+            }
+            else if(weekDay == "Wednesday"){
+                System.out.println("This is the 3rd day of the week.");
+            }
+            else if(weekDay == "Thursday"){
+                System.out.println("This is the 4th day of the week.");
+            }
+            else if(weekDay == "Friday"){
+                System.out.println("This is the 5th day of the week.");
+            }
+        }
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            Console con = System.console();
+            if(con == null)
+            {
+                System.out.println("No console");
+                return;
+            }
+            
+            //Problem 1
+            System.out.println("_1st Question_");
+            System.out.print("Please enter a number: ");
+                num1 = sc.nextInt();
+                MathSigns();
+            System.out.println(" ");
+    
+            //Problem 2
+            System.out.println("_2nd Question_");
+            System.out.print("Please enter a number: ");
+                correctNumber = sc.nextInt();
+                NumberHunt();
+            System.out.println(" ");
+        
+            //Problem 3
+            System.out.println("_3rd Question_");
+            System.out.print("Enter the number you want to be negated: ");
+                negatedNum = sc.nextInt();
+                CursedTechniqueNumberNegation();
+            System.out.println(" ");
+    
+            //Problem 4
+            System.out.println("_4th Question_");
+                weekDay = con.readLine("Please enter a day of the week: ");
+                WeekDays();
+            System.out.println(" ");
+    
+    
+    
+    
+    
+    }
+
+
+}
+
+
