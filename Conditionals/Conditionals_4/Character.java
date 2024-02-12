51f3d300efc8ae705bc07666cda206975eafecd6
KamDaDaddy <97680346+KamDaDaddy@users.noreply.github.com>
2024-02-12 10:09:57 -0500

    Outline for Conditionals 4 created

---
+++ Conditionals/Conditionals_4/Character.java
@@ -0,0 +1,13 @@
+package Conditionals_4;
+
+public class Character {
+    public String firstName;
+    public String lastName;
+    public String powers;
+
+    public Character(String f, String l, String p){
+        this.firstName = f;
+        this.lastName = l;
+        this.powers = p;
+    }
+}
---
+++ Conditionals/Conditionals_4/Conditionals_4.java
@@ -0,0 +1,12 @@
+package Conditionals_4;
+
+public class Conditionals_4 {
+    public static void main(String[] args) {
+        Character person1 = new Character("Peter", "Parker", "Enhanced Abilities");
+        Character person2 = new Character("Gwendolyne", "Stacy", "Enhanced Abilities");
+        Character person3 = new Character("Hobart", "Brown", "Enhanced Abilities");
+
+        Character person4 = person1;
+        Character person5 = person2;
+    }
+}
