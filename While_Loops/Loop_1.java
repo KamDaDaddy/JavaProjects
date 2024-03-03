cf5a63382e9e3c30e99406504013d6a61dd2e076
KamDaDaddy <kreyes24@aoiths.org>
2024-03-03 15:12:50 -0500

    Completed :)

--- While_Loops/Loop_1.java
+++ While_Loops/Loop_1.java
@@ -2,9 +2,6 @@ package While_Loops;
 import java.util.Scanner;
 
 public class Loop_1 {
-    static int num1;
-    static int num2;
-    
     public static void Asterisks(){
         int asterisks = 1;
 
@@ -16,32 +13,74 @@ public class Loop_1 {
     }
     
     
-    //Problem 2
-     public static void SumCalculator(){
+    //Problem 2 & 5
+    public static void SumCalculator(){
         int sum = 0;
-        int userNum = 1;
-        while (userNum <= 20){
-            sum += userNum;
-            userNum++;
+        int num1 = 1;
+
+        while (num1 <= 20){
+            sum += num1;
+            if(num1 == 1){
+                System.out.println("0 + " + num1 +  " = " + sum);
+            }
+            else {
+                System.out.println((sum - num1) + " + " + num1 + " = " + sum);
+            }
+        
+            num1++;
         }
-        System.out.println();
     }
     
+    //Problem 3
+    public static void NumberLadder(){
+        int start = 50;
+        int end = 20;
+
+        while (start >= end){
+            System.out.println(start);
+            start--;
+        }
+    
+        System.out.println();
+    }
+
+    //Problem 4
+    public static void Multiply(){
+        int term1 = 12;
+        int term2 = 15;
+        
+        int result;
+
+        while (term1 >= 0){
+            result = term1 * term2;
+            System.out.println(term1 + " x " + term2 + " = " + result);
+            term2--;
+
+            if(term2 < 0){
+                break;
+            }
+        }
+    }
+
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        //Problem two
-        System.out.println("Enter a number: ");
-        num1 = sc.nextInt();
-        System.out.println("Enter a second number: ");
-        num2 = sc.nextInt();
-
+        //Problem two and five
+        System.out.println("Problem two and five sums");
         SumCalculator();
+        System.out.println();
 
+        //Problem three
+        System.out.println("Problem three Number Ladder!");
+        NumberLadder();
+        System.out.println();
+
+        //Problem four
+        System.out.println("Problem four multiplying!");
+        Multiply();
+        System.out.println();
         
-        
-        
-    
     
     
     
