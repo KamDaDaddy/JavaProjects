3b0afb02017cba8f4094aabc31d0cea13d5de471
KamDaDaddy <kreyes24@aoiths.org>
2024-03-03 14:24:47 -0500

    Finished Conditionals 4

--- Conditionals/Conditionals_4/Conditionals_4.java
+++ Conditionals/Conditionals_4/Conditionals_4.java
@@ -2,16 +2,66 @@ package Conditionals_4;
 
 public class Conditionals_4 {
     public static void main(String[] args) {
-        Character person1 = new Character("Peter", "Parker", "Enhanced Abilities");
-        Character person2 = new Character("Gwendolyne", "Stacy", "Enhanced Abilities");
-        Character person3 = new Character("Hobart", "Brown", "Enhanced Abilities");
+        Character PParker = new Character("Peter", "Parker", "Enhanced Abilities");
+        Character GStacy = new Character("Gwendolyne", "Stacy", "Enhanced Abilities");
+        Character HBrown = new Character("Hobart", "Brown", "Enhanced Abilities");
 
-        Character person4 = person1;
-        Character person5 = person2;
+        Character Extra1 = PParker;
+        Character Extra2 = GStacy;
     
+        
+        //Problem one using ==:
+        if(PParker == Extra1){
+            System.out.println("Both Spideers are from the same universe");
+        }
+        else {
+            System.out.println("Spideers are from a different universe");
+        }
     
-        if()
+        //Problem one using .equals:
+        if (PParker.equals(Extra1)) {
+            System.out.println("Spideers are from the same universe");
+        }
+        else {
+            System.out.println("Spideers are from a different universe");
+        }
+        
+        //----------------------------------------------
+
+        //Problem two using ==:
+        if (HBrown == PParker) {
+            System.out.println("Spideers are from the same universe");
+        }
+        else {
+            System.out.println("Spideers are from a different universe");
+        }
     
+        //Problem two using .equals:
+        if (HBrown.equals(PParker)) {
+            System.out.println("Spideers are from the same universe");
+        }
+        else {
+            System.out.println("Spideers are from a different universe");
+        }
     
+        //-----------------------------------------------
+    
+        //Problem three using ==:
+        if (GStacy == PParker) {
+            System.out.println("Spideers are from the same universe");
+        }
+        else {
+            System.out.println("Spideers are from a different universe");
+        }
+    
+        //Problem three using .equals
+        if (GStacy.equals(PParker)) {
+            System.out.println("Spideers are from the same universe");
+        }
+        else {
+            System.out.println("Spideers are from a different universe");
+        }
     }
+    
+    
 }
