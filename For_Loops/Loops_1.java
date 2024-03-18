f6bb39e9c561141acb43e143d9eb41e9fdc283b7
KamDaDaddy <97680346+KamDaDaddy@users.noreply.github.com>
2024-03-18 14:56:11 -0400

    oop

---
+++ For_Loops/Loops_1.java
@@ -0,0 +1,37 @@
+package For_Loops;
+
+public class Loops_1 {
+    
+    //Question 1
+    /*
+    public static void Numbers(){
+        for(int i = 1; i <= 10; i++){
+            System.out.print(i + " ");
+        }
+    }
+    */
+    
+    //Question 2
+    public static void Countdown(){
+        
+    }
+    
+    
+    public static void main(String[] args){
+
+        //Question 1
+        //Numbers();
+        System.out.println(" ");
+
+        //Question 2
+        Countdown();
+
+        //Question 3
+
+
+        //Question 4
+
+
+    }
+}   
+
