48b3a4ab1823b49aa95636f3b65435e19708d826
KamDaDaddy <97680346+KamDaDaddy@users.noreply.github.com>
2024-03-19 11:00:57 -0400

    woop

--- Nested_Interations.java
+++ Nested_Interations.java
@@ -1,7 +1,29 @@
 public class Nested_Interations {
     public static void main(String[] args) {
-        for (int i = 0; i <= 10; i++){
-            System.out.print("*");
+        
+        //A. 
+        /*
+        for(int line = 0; line < 4; line++){
+            System.out.println(" ");
+            
+            for(int star = 0; star <= 10; star++){
+                System.out.print("*");
+            }
         }
+        */
+    
+        //B. 
+        for(int stair = 0; stair < 7; stair++){
+            System.out.println("");
+
+            for(int asterisks = 7; asterisks >= 1; asterisks--){
+                System.out.print("*");
+            
+            }
+            
+        }
+    
+    
+    
     }
 }
